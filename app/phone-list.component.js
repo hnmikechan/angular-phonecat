714acd5c6ed8ff9336677b9511dd246a927d2364
Michael Kielb <hnmikechan@gmail.com>
2017-04-16 18:23:32 -0400

    Mike Finished Step 3

--- app/phone-list.component.js
+++ app/phone-list.component.js
@@ -5,13 +5,24 @@ angular.
   module('phonecatApp').
   component('phoneList', {
     template:
+        'Hello, {{$ctrl.name}}!' +  //added by Mike
+        '<p>Total Number of Phones: {{$ctrl.phones.length}}</p>'  +  //added by Mike
         '<ul>' +
           '<li ng-repeat="phone in $ctrl.phones">' +
             '<span>{{phone.name}}</span>' +
             '<p>{{phone.snippet}}</p>' +
           '</li>' +
-        '</ul>',
+        '</ul>' +
+        '<table>' +  //added by Mike
+            '<tr><th style="border: 1px solid black; padding: 2px;" ng-repeat="j in [0,1,2,3,4,5,6,7]">Row {{j}}</th></tr>' +  //added by Mike
+            '<tr ng-repeat="i in [0, 1, 2, 3, 4, 5, 6, 7]">' +  //added by Mike
+                '<td style="border: 1px solid black;padding: 2px" ng-repeat="k in [0,1,2,3,4,5,6,7]">{{k}}</td>' +  //added by Mike
+            '</tr>' +  //added by Mike
+        '</table>',  //added by Mike
     controller: function PhoneListController() {
+      
+      this.name = 'world'; //added by Mike
+        
       this.phones = [
         {
           name: 'Nexus S',
